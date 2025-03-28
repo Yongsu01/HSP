22de37bf46df816258f5f192364ec275bbc948f5
CHO YONGSU <choyongsu2001@naver.com>
2025-03-29 03:31:33 +0900

    tailwind css 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
-
+import "@/app/globals.css"; 
 
 export const metadata: Metadata = {
   title: "FitU",
@@ -13,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html>
       <body>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      DSA
+    <div className="text-blue-500 text-4xl font-bold">
+      dsadds
     </div>
   );
 }
