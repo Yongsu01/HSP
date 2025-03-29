569f03389bdd6cf285457fc02deb8b5e97ba9f0d
CHO YONGSU <choyongsu2001@naver.com>
2025-03-29 23:39:32 +0900

    feat:로그인 레이아웃 #1

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,8 +1,13 @@
-export default function Background(){
-return(
+import React from "react"
 
-    <div className="">
+type BackgroundProps =  {
+    children : React.ReactNode;
+}
 
-    </div>
-)
+export default function Background({children}: BackgroundProps) {
+  return( 
+    <div className="w-full h-screen bg-[#4B4B4B] flex flex-col items-center pt-[50px] pb-[10vh]">
+    {children}
+  </div>
+  )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+import Image from "next/image";
+import Kakao from "../../../public/Kakao.svg";
 export default function Home() {
-    return (
-      <div className=" ">
+  return (
+    <div className="h-[100%]">
+      <div className="text-[#FFFFFF] text-[25px] underline pt-[120px] h-[100%] flex flex-col items-center justify-between">
+        <div className="flex flex-col items-center gap-[30px]" >
+          LOG IN
+          <Image src={Kakao} alt="Kakao" onClick={()=>{console.log("dsa")}}></Image>
+        </div>
+        <div className=" text-[#C0C0C0] text-[17px] underline decoration-[#C0C0C0] pb-[30px]">
+          헬린이를 위한 FitU
+        </div>
       </div>
-    );
-  }
-  
+      </div>
+  )
+}
