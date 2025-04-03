f4fc835958d39a75afe679142a4485049b49c686
CHO YONGSU <choyongsu2001@naver.com>
2025-04-03 20:13:19 +0900

    Rename

--- src/app/login/components/KakaoLogin.tsx
+++ src/app/login/components/KakaoLogin.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Kakao from "../../../../../public/Kakao.svg"
+import Kakao from "../../../../public/Kakao.svg"
 function KakaoLogin(){
     const REST_API_KEY =  process.env.REACT_APP_REST_API_KEY;
     const REDIRECT_URI = 'http://localhost:3000/login/Oauth'
