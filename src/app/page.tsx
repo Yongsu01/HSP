74d0c2ceaa68d38eb27a957104cb015f944f6f33
CHO YONGSU <choyongsu2001@naver.com>
2025-04-03 20:26:54 +0900

    feat:로그인 안했을시 로그인 페이지로 이동 #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-export default function Home() {
-  return (
-    <div>
+"use client"; 
 
-      DSA
-    </div>
-  );
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  const token = sessionStorage.getItem("authorization")
+  useEffect(()=>{
+    if(token){
+      router.push('/main');
+    }else{
+      router.push('/login');
+    }
+  },[])
+
+  return(
+    <div>...</div>
+  )
 }
