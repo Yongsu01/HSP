72d6633cac6671f903fa90174bab4d6b1f8b41cf
CHO YONGSU <choyongsu2001@naver.com>
2025-04-22 19:26:12 +0900

    feat:메인페이지 레이아웃 #11

---
+++ src/app/mainpage/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+const MainPage: React.FC = () => {
+
+  return (
+    <div className="text-white text-[20px] pt-[50px]">
+      운동할 부위를 골라주세요!
+   </div>
+  );
+};
+
+export default MainPage;
