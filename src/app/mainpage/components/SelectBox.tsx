c0053835f889081fcc734e34d9f2db266062cbb7
CHO YONGSU <choyongsu2001@naver.com>
2025-04-23 17:10:41 +0900

    feat:메인페이지 레이아웃 #11

---
+++ src/app/mainpage/components/SelectBox.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+type SelectBoxProps = {
+  selected: string[]; 
+  onChange: (selected: string[]) => void; 
+};
+
+const BodyPart: Option[] = [
+  { label: '어깨', value: 'shoulder' },
+  { label: '가슴', value: 'chest' },
+  { label: '등', value: 'back' },
+  { label: '팔', value: 'arm' },
+  { label: '하체', value: 'leg' },
+];
+
+const SelectBox: React.FC<SelectBoxProps> = ({ selected, onChange }) => {
+  const handleCheckboxChange = (value: string) => {
+    if (selected.includes(value)) {
+      onChange(selected.filter((v) => v !== value)); // 제거
+    } else {
+      onChange([...selected, value]); // 추가
+    }
+  };
+
+  return (
+    <div className="text-white flex flex-col gap-3 mt-4">
+    {BodyPart.map((part) => (
+      <label
+        key={part.value}
+        className="flex items-center gap-3 bg-[#1f2937] p-3 rounded-xl cursor-pointer transition hover:bg-[#374151]"
+      >
+        <input
+          type="checkbox"
+          value={part.value}
+          checked={selected.includes(part.value)}
+          onChange={() => handleCheckboxChange(part.value)}
+          className="w-5 h-5 accent-[#E45258] rounded-md"
+        />
+        <span className="text-[16px] font-medium">{part.label}</span>
+      </label>
+    ))}
+  </div>
+  );
+};
+
+export default SelectBox;
