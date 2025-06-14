258a68f167d78ca2ed530184c7c1359887a4a4bf
CHO YONGSU <choyongsu2001@naver.com>
2025-06-14 17:47:23 +0900

    HOTFIX

--- src/app/login/Oauth/page.tsx
+++ src/app/login/Oauth/page.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from "react";
 import OAuthLogin from "./OauthLogin";
 
-export default function Home() {
+export default function Page() {
   return (
-    <Suspense>
+    <Suspense fallback={<div className="text-center mt-10">로그인 중입니다...</div>}>
       <OAuthLogin />
     </Suspense>
   );
