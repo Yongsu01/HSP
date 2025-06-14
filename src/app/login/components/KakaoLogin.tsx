a4f2e8ef2b3c44461daceb80f81343be69155d45
CHO YONGSU <choyongsu2001@naver.com>
2025-06-14 16:53:03 +0900

    fix:배포 에러 해결

--- src/app/login/components/KakaoLogin.tsx
+++ src/app/login/components/KakaoLogin.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Kakao from "../../../../public/Kakao.svg"
 function KakaoLogin(){
     const REST_API_KEY =  process.env.NEXT_PUBLIC_IP_API_KEY;
-    const REDIRECT_URI = 'http://localhost:3000/login/Oauth'
+    const REDIRECT_URI = 'https://hsp-sigma.vercel.app/login/Oauth'
     const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
     const LoginHandler =()=>{
