54869deeef9d2865ae2dca497e36601e193bc20d
CHO YONGSU <choyongsu2001@naver.com>
2025-06-14 16:44:27 +0900

    fix:배포 에러 해결

---
+++ src/app/login/Oauth/OauthLogin.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
+
+
+const OAuthLogin = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+
+  useEffect(() => {
+    if (code) {
+      const fetchData = async () => {
+        try {
+          const response = await getPrintCode(code);
+            console.log(response);
+          const jwtToken = response.data["token"];
+          if (jwtToken) sessionStorage.setItem("Authorization", jwtToken);
+
+          const refreshToken = response.data["token"];
+          if (refreshToken) sessionStorage.setItem("RefreshToken", refreshToken);
+
+          if (response.status === 201) {
+            router.push("/login/moreinfo");
+          } else if (response.status === 200) {
+            router.push("/mainpage");
+          }
+        } catch (error) {
+        }
+      };
+
+      fetchData();
+    } else {
+      router.push("/login");
+    }
+  }, []);
+
+  async function getPrintCode(code: string) {
+
+    const res = await axios.post("http://ec2-3-35-143-24.ap-northeast-2.compute.amazonaws.com:8080/auth/login/kakao",null,
+      {params: {code:code}}
+    );
+    
+    return res;
+  }
+
+  return (
+     <div className="flex items-center justify-center h-screen">
+      <div className="w-12 h-12 border-4 border-[#E45258] border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default OAuthLogin;
--- src/app/login/Oauth/page.tsx
+++ src/app/login/Oauth/page.tsx
@@ -1,56 +1,11 @@
-"use client";
+// app/exercise/detail/page.tsx
+import { Suspense } from "react";
+import OAuthLogin from "./OauthLogin";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import axios from "axios";
-
-
-const OAuthLogin = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
-
-  useEffect(() => {
-    if (code) {
-      const fetchData = async () => {
-        try {
-          const response = await getPrintCode(code);
-            console.log(response);
-          const jwtToken = response.data["token"];
-          if (jwtToken) sessionStorage.setItem("Authorization", jwtToken);
-
-          const refreshToken = response.data["token"];
-          if (refreshToken) sessionStorage.setItem("RefreshToken", refreshToken);
-
-          if (response.status === 201) {
-            router.push("/login/moreinfo");
-          } else if (response.status === 200) {
-            router.push("/mainpage");
-          }
-        } catch (error) {
-        }
-      };
-
-      fetchData();
-    } else {
-      router.push("/login");
-    }
-  }, []);
-
-  async function getPrintCode(code: string) {
-
-    const res = await axios.post("http://ec2-3-35-143-24.ap-northeast-2.compute.amazonaws.com:8080/auth/login/kakao",null,
-      {params: {code:code}}
-    );
-    
-    return res;
-  }
-
+export default function DetailPage() {
   return (
-     <div className="flex items-center justify-center h-screen">
-      <div className="w-12 h-12 border-4 border-[#E45258] border-t-transparent rounded-full animate-spin"></div>
-    </div>
+    <Suspense>
+      <OAuthLogin />
+    </Suspense>
   );
-};
-
-export default OAuthLogin;
+}
